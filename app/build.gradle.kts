d49fcd68f5339aab75f192165ad1e1866d3752d5
cristmasbox <sonne.zucker@gmx.de>
2026-01-29 20:24:27 +0100

    new version set

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.blueapps.egyptianwriter"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "10.01.2025@0.0.2"
+        versionCode = 3
+        versionName = "29.01.2026@0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
